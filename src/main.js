ca7ea19dfd7280292af3492b32960581e73fabc2
Lucas Barcelos <lucas.barcelos@signa.net.br>
2020-03-06 16:30:49 -0300

    subindo Pwa

--- src/main.js
+++ src/main.js
@@ -11,6 +11,7 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import router from './router/'
 import store from './store/'
+import './registerServiceWorker'
 
 Vue.config.productionTip = false
 
